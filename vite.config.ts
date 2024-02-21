9195df20f3acfcbe784f42f6b8102e217bf843fb
pdy <1412656741@qq.com>
2024-02-21 14:50:37 +0800

    修改js文件baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Inspect from 'vite-plugin-inspect'
 import path from 'node:path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [Inspect(),vue()],
-  resolve:{
-    alias:{
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server:{
-    host:true
-  }
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    plugins: [Inspect(),vue()],
+    resolve:{
+      alias:{
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    server:{
+      host:true
+    },
+    base: env.VITE_BASE_URL,
+  })
+}
